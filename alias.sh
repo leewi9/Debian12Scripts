954fd8a5fa758777bf6e7000193f66f4eab3e3cb
leewi9 <leewi9@yahoo.com>
2024-04-26 15:42:27 +0800

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -4,5 +4,6 @@ echo "alias ll='ls -alFh'" >> ~/.bashrc
 echo "alias la='ls -alFh'" >> ~/.bashrc
 echo "alias l='ls -alFh'" >> ~/.bashrc
 
+echo "alias h='history'" >> ~/.bashrc
 echo "alias hg='history | grep '" >> ~/.bashrc
 echo "alias pg='ps -ef | grep '" >> ~/.bashrc
